ad8146165debc2bc260ebd5d67dbc5178eb864b3
Serhii Matiushenko <sergdpi@gmail.com>
2022-01-16 22:53:59 +0200

    Demo

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ def getLocalTime():
 
 @app.route('/')
 def home():
+    greeting_string = 'Please, update this string N13 for run CI, '
     build_version = os.environ['BuildVersion']
     name = os.environ['MyName']
     ltime = getLocalTime()
-    return 'Build version: ' + build_version  + '\n' + '<br/>' + 'Hello, ' + name  + '\n' + '<br/>' +  'The current time on a server is: ' + str(ltime)
+    return 'Build version: ' + build_version  + '\n' + '<br/>' + greeting_string + name  + '\n' + '<br/>' +  'The current time on a server is: ' + str(ltime)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
